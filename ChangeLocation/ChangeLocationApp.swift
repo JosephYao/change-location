acef4122ee64fb7ef08fc71c2df43a977cb20410
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-12-01 10:39:34 +0800

    Initial Commit

---
+++ ChangeLocation/ChangeLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeLocationApp.swift
+//  ChangeLocation
+//
+//  Created by Joseph Yao on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct ChangeLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
